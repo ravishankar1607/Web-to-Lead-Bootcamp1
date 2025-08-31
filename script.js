349897deed4195344d243767574197bbd42f9dea
Ravi Shankar <shankar.revival.ravi@gmail.com>
2025-08-31 22:18:53 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function beforesubmit() {
+  debugger;
+  let outputdate = document.querySelector(".outputdate");
+  let inputdate = document.querySelector(".inputdate");
+  console.log("inputdate.value", inputdate.value);
+
+  //string --> date (en_US)
+  let formattedDate = new Date(inputdate.value).toLocaleDateString("en-US");
+  outputdate.value = formattedDate;
+}
