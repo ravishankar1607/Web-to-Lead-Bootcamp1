111471ed1059b9a1cc05536865ed0b1e574bb511
Ravi Shankar <shankar.revival.ravi@gmail.com>
2025-08-31 23:14:04 +0530

    adding validation for recaptcha

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-function beforesubmit() {
-  debugger;
-  let outputdate = document.querySelector(".outputdate");
-  let inputdate = document.querySelector(".inputdate");
-  console.log("inputdate.value", inputdate.value);
+let captchachecked = false;
+function beforesubmit(event) {
+  if (captchachecked) {
+    let outputdate = document.querySelector(".outputdate");
+    let inputdate = document.querySelector(".inputdate");
+    console.log("inputdate.value", inputdate.value);
 
-  //string --> date (en_US)
-  let formattedDate = new Date(inputdate.value).toLocaleDateString("en-US");
-  outputdate.value = formattedDate;
+    //string --> date (en_US)
+    let formattedDate = new Date(inputdate.value).toLocaleDateString("en-US");
+    outputdate.value = formattedDate;
+  } else {
+    alert("Please check the reCAPTCHA box to submit the lead.");
+    event.preventDefault();
+  }
 }
 
 function timestamp() {
@@ -21,3 +26,7 @@ function timestamp() {
   }
 }
 setInterval(timestamp, 500);
+
+function captchasuccess() {
+  captchachecked = true;
+}
